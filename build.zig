bf9a15a76aa54473ba14586124b30f8a5e636b13
utox39 <francescomoccaldi39@gmail.com>
2025-03-06 14:45:58 +0100

    build: link libc for linux

--- build.zig
+++ build.zig
@@ -41,6 +41,10 @@ pub fn build(b: *std.Build) void {
         exe.linkFramework("IOKit");
     }
 
+    if (target.result.os.tag == .linux) {
+        exe.linkLibC();
+    }
+
     // This declares intent for the executable to be installed into the
     // standard location when the user invokes the "install" step (the default
     // step when running `zig build`).
