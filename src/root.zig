0dfc050fbe4bc35bcbb25836c75a23d43c9b40a1
utox39 <francescomoccaldi39@gmail.com>
2025-03-18 10:45:06 +0100

    refactor: rename root.zig to detection.zig

--- src/root.zig
+++
@@ -1,8 +0,0 @@
-const std = @import("std");
-
-pub const os_module = switch (@import("builtin").os.tag) {
-    .linux => @import("linux/linux.zig"),
-    .macos => @import("macos/macos.zig"),
-    .windows => @import("windows/windows.zig"),
-    else => @compileError("Unsupported operating system"),
-};
