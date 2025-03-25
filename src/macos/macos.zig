58473d07eedd3b6bf9a3771ea898d8d3f1af881f
utox39 <francescomoccaldi39@gmail.com>
2025-03-25 11:09:19 +0100

    feat(macos): add import

--- src/macos/macos.zig
+++ src/macos/macos.zig
@@ -2,3 +2,4 @@ pub const hardware = @import("./hardware.zig");
 pub const network = @import("./network.zig");
 pub const system = @import("./system.zig");
 pub const user = @import("./user.zig");
+pub const packages = @import("./packages.zig");
