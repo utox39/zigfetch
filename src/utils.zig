53a5edc65eb5935a38ec505441cc57bf7c32f0ce
utox39 <francescomoccaldi39@gmail.com>
2026-08-08 14:47:04 +0200

    feat(utils): add a utility function to check whether the Kitty protocol is supported

--- src/utils.zig
+++ src/utils.zig
@@ -140,6 +140,59 @@ pub fn countEntries(io: std.Io, dir_path: []const u8) !usize {
     return count;
 }
 
+/// Checks whether the terminal supports the Kitty graphics protocol.
+///
+/// Sends a graphics protocol query immediately followed by a primary device
+/// attributes query, then reads the response. If a reply to the graphics
+/// query arrives (an APC sequence starting with `ESC _G`), the terminal
+/// supports the protocol. If only the device attributes response (`ESC [ ... c`)
+/// comes back, it does not.
+pub fn supportsKittyProtocol(io: std.Io) bool {
+    // TODO: Handle Windows as soon as it is supported
+
+    if (builtin.os.tag != .linux and builtin.os.tag != .macos) return false;
+
+    const stdin = std.Io.File.stdin();
+    const stdout = std.Io.File.stdout();
+
+    if (!(stdin.isTty(io) catch false) or !(stdout.isTty(io) catch false)) return false;
+
+    const original_termios = std.posix.tcgetattr(stdin.handle) catch return false;
+    var raw_termios = original_termios;
+    raw_termios.lflag.ICANON = false;
+    raw_termios.lflag.ECHO = false;
+    raw_termios.cc[@intFromEnum(std.posix.V.MIN)] = 0;
+    raw_termios.cc[@intFromEnum(std.posix.V.TIME)] = 1; // 100 ms
+
+    std.posix.tcsetattr(stdin.handle, .NOW, raw_termios) catch return false;
+    defer std.posix.tcsetattr(stdin.handle, .NOW, original_termios) catch {};
+
+    const query = "\x1b_Gi=31,s=1,v=1,a=q,t=d,f=24;AAAA\x1b\\\x1b[c";
+    stdout.writeStreamingAll(io, query) catch return false;
+
+    var response_buf: [128]u8 = undefined;
+    var len: usize = 0;
+
+    while (len < response_buf.len) {
+        var byte: [1]u8 = undefined;
+        var bufs = [_][]u8{&byte};
+        const n = stdin.readStreaming(io, &bufs) catch break;
+        if (n == 0) break;
+
+        response_buf[len] = byte[0];
+        len += 1;
+
+        // Stop once the primary device attributes response (`ESC [ ... c`) is complete
+        if (byte[0] == 'c') break;
+    }
+
+    return std.mem.indexOf(u8, response_buf[0..len], "\x1b_G") != null;
+}
+
+test "supportsKittyProtocol" {
+    std.debug.print("supportsKittyProtocol: {any}\n", .{supportsKittyProtocol(std.testing.io)});
+}
+
 /// Checks whether the file is a PNG image by inspecting its magic bytes.
 pub fn isFilePng(gpa: std.mem.Allocator, io: std.Io, file_path: []const u8) !bool {
     const png_magic = [8]u8{ 0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A };
