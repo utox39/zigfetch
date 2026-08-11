0ac832f6552b79fa72d4c6b926dd9853b79fd21d
utox39 <francescomoccaldi39@gmail.com>
2026-08-11 14:02:02 +0200

    build(build.zig): update build system to 0.17

--- build.zig
+++ build.zig
@@ -73,9 +73,10 @@ pub fn build(b: *std.Build) void {
 
     // This allows the user to pass arguments to the application in the build
     // command itself, like this: `zig build run -- arg1 arg2 etc`
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
-    }
+    run_cmd.addPassthruArgs();
+    // if (b.args) |args| {
+    //     run_cmd.addArgs(args);
+    // }
 
     // This creates a build step. It will be visible in the `zig build --help` menu,
     // and can be selected like this: `zig build run`
