7669b468b516d95cbef0fb3aac2a100e74a91362
utox39 <francescomoccaldi39@gmail.com>
2026-09-09 14:28:27 +0200

    feat(linux-packages): add RPM support

--- src/linux/packages.zig
+++ src/linux/packages.zig
@@ -1,5 +1,7 @@
 const std = @import("std");
 const utils = @import("../utils.zig");
+const build_options = @import("build_options");
+const c = @import("c");
 
 pub fn getPackagesInfo(gpa: std.mem.Allocator, io: std.Io, environ: std.process.Environ) ![]const u8 {
     var packages_info = std.array_list.Managed(u8).init(gpa);
@@ -10,6 +12,11 @@ pub fn getPackagesInfo(gpa: std.mem.Allocator, io: std.Io, environ: std.process.
     const dpkg_packages = countDpkgPackages(gpa, io) catch |err| if (err == error.FileNotFound) 0 else return err;
     const pacman_packages = countPacmanPackages(io) catch |err| if (err == error.FileNotFound) 0 else return err;
     const xbps_packages = countXbpsPackages(gpa, io) catch |err| if (err == error.FileNotFound) 0 else return err;
+    // If sqlite fails it fallback to librpm.
+    // Inspired by: https://github.com/fastfetch-cli/fastfetch/blob/5d290cc3d5c61ee23f7f1ffa04e54c18254984af/src/detection/packages/packages_linux.c#L649
+    //
+    // Errors from `countRpmPackagesViaSqlite` are ignored for now
+    const rpm_packages = countRpmPackagesViaSqlite("/var/lib/rpm/rpmdb.sqlite") catch countRpmPackagesViaLibrpm();
 
     var buffer: [32]u8 = undefined;
 
@@ -31,6 +38,10 @@ pub fn getPackagesInfo(gpa: std.mem.Allocator, io: std.Io, environ: std.process.
 
     if (xbps_packages > 0) {
         try packages_info.appendSlice(try std.fmt.bufPrint(&buffer, " Xbps: {d}", .{xbps_packages}));
+    }
+
+    if (rpm_packages > 0) {
+        try packages_info.appendSlice(try std.fmt.bufPrint(&buffer, " RPM: {d}", .{rpm_packages}));
     }
 
     return try gpa.dupe(u8, packages_info.items);
@@ -251,4 +262,76 @@ fn countXbpsPackages(gpa: std.mem.Allocator, io: std.Io) !usize {
 
     // Subtruct 1 to remove an useless line
     return count - 1;
+}
+
+// Inspired by: https://github.com/fastfetch-cli/fastfetch/blob/5d290cc3d5c61ee23f7f1ffa04e54c18254984af/src/detection/packages/packages_linux.c#L188
+fn countRpmPackagesViaLibrpm() usize {
+    if (build_options.enable_rpm) {
+        // Don't print any error messages
+        //
+        // rpmlogSetMask(RPMLOG_MASK(RPMLOG_EMERG));
+        //
+        // #define RPMLOG_MASK(pri) (1 << ((unsigned)(pri)))
+        //
+        _ = c.rpmlogSetMask(1 << @as(usize, @intCast(c.RPMLOG_EMERG)));
+
+        if (c.rpmReadConfigFiles(null, null) != 0) {
+            return 0;
+        }
+
+        const ts: c.rpmts = c.rpmtsCreate();
+        if (ts == null) {
+            return 0;
+        }
+
+        const match_iter: c.rpmdbMatchIterator = c.rpmtsInitIterator(ts, c.RPMDBI_LABEL, null, 0);
+        if (match_iter == null) {
+            _ = c.rpmtsFree(ts);
+            return 0;
+        }
+
+        const count: usize = @intCast(c.rpmdbGetIteratorCount(match_iter));
+
+        _ = c.rpmdbFreeIterator(match_iter);
+        _ = c.rpmtsFree(ts);
+
+        return count;
+    }
+
+    return 0;
+}
+
+fn countRpmPackagesViaSqlite(path: []const u8) !usize {
+    if (build_options.enable_rpm) {
+        var db_connection: ?*c.sqlite3 = undefined;
+        if (c.sqlite3_open(path.ptr, &db_connection) != c.SQLITE_OK) {
+            // std.log.err("Can't open database {s}: {s}\n", .{ path, c.sqlite3_errmsg(db_connection) });
+            return error.FailedToOpenTheRpmDB;
+        }
+        defer _ = c.sqlite3_close(db_connection);
+
+        const query =
+            \\ SELECT COUNT(*) FROM Sigmd5;
+        ;
+
+        var stmt: ?*c.sqlite3_stmt = undefined;
+        if (c.sqlite3_prepare_v2(db_connection, query, @as(c_int, @intCast(query.len + 1)), &stmt, null) != c.SQLITE_OK) {
+            // std.log.err("Can't create prepared statement: {s}\n", .{c.sqlite3_errmsg(db_connection)});
+            return error.FailedToPrepareTheStatementForTheRpmDB;
+        }
+        defer _ = c.sqlite3_finalize(stmt);
+
+        var count: usize = 0;
+        var rc = c.sqlite3_step(stmt);
+        while (rc == c.SQLITE_ROW) : (rc = c.sqlite3_step(stmt)) {
+            count = @as(usize, @intCast(c.sqlite3_column_int(stmt, 0)));
+        } else if (rc != c.SQLITE_DONE) {
+            // std.log.err("Failed to get row: {s}\n", .{c.sqlite3_errmsg(db_connection)});
+            return error.FailedToGetRpmPackagesCount;
+        }
+
+        return count;
+    }
+
+    return 0;
 }
